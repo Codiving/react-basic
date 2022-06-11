b43f9d75123da251246a4f81f9920c7101b6eb0e
Codiving <codivingmoon@gmail.com>
2022-06-11 19:03:52 +0900

    useRef 실습 코드

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
-import ObjectType from "./state/ObjectType";
-import Rendered from "./state/Rendered";
-import Unrendered from "./state/Unrendered";
+import Controlled from "./ref/Controlled";
+import Focus from "./ref/Focus";
+import Uncontrolled from "./ref/Uncontrolled";
 
 const App = () => {
   return (
     <>
-      <Rendered />
-      <Unrendered />
-      <ObjectType />
+      <div>
+        <Focus />
+        <br />
+      </div>
+      <hr />
+      <div style={{ marginTop: 100 }}>
+        <div>
+          <Controlled />
+        </div>
+        <div style={{ marginTop: 50 }}>
+          <Uncontrolled />
+        </div>
+      </div>
     </>
   );
 };
