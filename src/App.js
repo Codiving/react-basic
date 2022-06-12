b635a49b1901aa41a3c161a074b786495c5b33d7
Codiving <codivingmoon@gmail.com>
2022-06-12 15:00:45 +0900

    UseEffect 실습

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
 import CountTimer from "./useEffect/practice/CountTimer";
+import RefCounter from "./useEffect/practice/RefCounter";
+import Resize from "./useEffect/practice/Resize";
+
+const style = {
+  marginBottom: 50,
+  padding: 10,
+  border: "1px solid"
+};
+
+const Components = [<Resize />, <CountTimer />, <RefCounter />];
 
 const App = () => {
-  return <CountTimer />;
+  return (
+    <>
+      {Components.map((Component, index) => (
+        <div key={index} style={style}>
+          {Component}
+        </div>
+      ))}
+    </>
+  );
 };
 
 export default App;
